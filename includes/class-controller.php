a5590e8f2de9bcd5a7869c23356cf8a5d034c304
Paul Clark <pdclark@brainstormmedia.com>
2013-10-28 21:38:38 -0800

    Add GPU_Controller class

---
+++ includes/class-controller.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Controller for the plugin.
+ * 
+ * @author Paul Clark <http://pdclark.com>
+ */
+class GPU_Controller {
+
+	/**
+	 * @var GPU_Controller Instance of this class.
+	 */
+	private static $instance = false;
+
+	/**
+	 * @var string Key for plugin options in wp_options table
+	 */
+	const OPTION_KEY = GPU_SLUG;
+
+	/**
+	 * @var int How often should transients be updated, in seconds.
+	 */
+	protected $update_interval;
+
+	/**
+	 * @var array Options from wp_options
+	 */
+	protected $options;
+
+	/**
+	 * @var GPU_Admin Admin object
+	 */
+	protected $admin;
+	
+	/**
+	 * Don't use this. Use ::get_instance() instead.
+	 */
+	public function __construct() {
+		if ( !self::$instance ) {
+			$message = '<code>' . __CLASS__ . '</code> is a singleton.<br/> Please get an instantiate it with <code>' . __CLASS__ . '::get_instance();</code>';
+			wp_die( $message );
+		}       
+	}
+
+	/**
+	 * If a variable is accessed from outside the class,
+	 * return a value from method get_$var()
+	 * 
+	 * For example, $inbox->unread_count returns $inbox->get_unread_count()
+	 * 
+	 * @return pretty-much-anything
+	 */
+	public function __get( $var ) {
+		$method = 'get_' . $var;
+
+		if ( method_exists( $this, $method ) ) {
+			return $this->$method();
+		}else {
+			return $this->$var;
+		}
+	}
+	
+	public static function get_instance() {
+		if ( !is_a( self::$instance, __CLASS__ ) ) {
+			self::$instance = true;
+			self::$instance = new self();
+			self::$instance->init();
+		}
+		return self::$instance;
+	}
+	
+	/**
+	 * Initial setup. Called by get_instance.
+	 */
+	protected function init() {
+
+		$this->options = get_site_option( self::OPTION_KEY );
+
+		// Filter allows search results to be updated more or less frequently.
+		// Default is 60 minutes
+		$this->update_interval = apply_filters( 'gpu_update_interval', 60*60 );
+
+	}
+
+	public function get_option( $key ) {
+		if ( isset( $this->options[ $key ] ) ) {
+			return $this->options[ $key ];
+		}else {
+			return false;
+		}
+	}
+
+	/**
+	 * Load HTML template from views directory.
+	 * Contents of $args are turned into variables for use in the template.
+	 * 
+	 * For example, $args = array( 'foo' => 'bar' );
+	 *   becomes variable $foo with value 'bar'
+	 */
+	public static function get_template( $file, $args = array() ) {
+		extract( $args );
+
+		include GPU_PLUGIN_DIR . "/views/$file.php";
+
+	}
+
+}
--- plugin.php
+++ plugin.php
@@ -1,23 +1,96 @@
 <?php
 /*
 Plugin Name: WP Github Plugin Updater
-Plugin URI: https://github.com/brainstormmedia/git-updater
+Plugin URI: https://github.com/brainstormmedia/git-plugin-updates
 Description: Update plugins hosted on <a href="http://github.com" target="_blank">Github</a> and <a href="http://bitbucket.org" target="_blank">Bitbucket</a>. Search and install plugins from Github using <a href="https://github.com/brainstormmedia/github-plugin-search/" target="_blank">Github Plugin Search</a>.
-Version: 0.1
-Real Version: 1.0
+Version: 1.0
 Author: Brainstorm Media
 Author URI: http://brainstormmedia.com/
 License: GPLv2
 */
 
 /**
- * NOTE: Version number above is 0.1 to test updates.
+ * ## TESTING
+ * 
+ * Change version number above to 0.1 to test updates.
+ * 
+ * ## Plugin and Theme Developers
+ * 
+ * You don't have to run Git Plugin Updates as a plugin!
+ * Instead, you can copy this plugin folder into your plugin or theme,
+ * then include this file:
+ * 
+ *   <code> require_once dirname( __FILE__ ) . '/git-plugin-updates/plugin.php'; </code>
+ * 
+ * ## CREDIT
  * 
  * Forked from WordPress Github Plugin Updater by Joachin Kudish
  * @link https://github.com/jkudish/WordPress-GitHub-Plugin-Updater
  * @link http://jkudish.com
  */
 
-define( 'WP_GITHUB_FORCE_UPDATE', WP_DEBUG );
+/**
+ * Used for localization text-domain, which must match wp.org slug.
+ * Used for wp-admin settings page slug.
+ * 
+ * @var string Slug of the plugin on wordpress.org.
+ */
+define( 'GPU_PLUGIN_SLUG', 'git-plugin-updates' );
 
-include_once dirname(__FILE__).'/updater.php';
+/**
+ * Used for error messages.
+ * Used for settings page title.
+ * 
+ * @var string Nice name of the plugin.
+ */
+define( 'GPU_PLUGIN_NAME', __( 'Git Plugin Updates', GPU_PLUGIN_SLUG ) );
+
+/**
+ * @var string Absolute path to this file.
+ */
+define( 'GPU_PLUGIN_FILE', __FILE__ );
+
+/**
+ * @var string Absolute path to the root plugin directory
+ */
+define( 'GPU_PLUGIN_DIR', dirname( __FILE__ ) );
+
+/**
+ * Load plugin dependencies and instantiate the plugin.
+ * Checks PHP version. Deactivates plugin and links to instructions if running PHP 4.
+ */
+function storm_git_plugin_updates_init() {
+	
+	// PHP Version Check
+	$php_is_outdated = version_compare( PHP_VERSION, '5.2', '<' );
+
+	// Only exit and warn if on admin page
+	$okay_to_exit = is_admin() && ( !defined('DOING_AJAX') || !DOING_AJAX );
+	
+	if ( $php_is_outdated ) {
+		if ( $okay_to_exit ) {
+			require_once ABSPATH . '/wp-admin/includes/plugin.php';
+			deactivate_plugins( __FILE__ );
+			wp_die( sprintf( __(
+				'%s requires PHP 5.2 or higher, as does WordPress 3.2 and higher. The plugin has now disabled itself. For information on upgrading, %ssee this article%s.', GHPS_PLUGIN_SLUG ),
+				GPU_PLUGIN_NAME,
+				'<a href="http://codex.wordpress.org/Switching_to_PHP5" target="_blank">',
+				'</a>'
+			) );
+		} else {
+			return;
+		}
+	}
+
+	// Be cautious, since this class might be included by multiple plugins.
+	if ( is_admin() && !class_exists( 'GPU_Controller') ) {
+
+		require_once dirname( __FILE__ ) . '/includes/class-controller.php';
+		
+		GPU_Controller::get_instance();
+
+	}
+
+}
+
+add_action( 'plugins_loaded', 'storm_git_plugin_updates_init' );
